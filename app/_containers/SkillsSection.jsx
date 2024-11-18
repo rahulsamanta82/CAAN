fc446bf7c8e456beead81ac281de0860854e16da
Rahul Samanta <145849509+rahulsamanta82@users.noreply.github.com>
2024-11-18 06:14:05 +0530

    Update SkillsSection.jsx

--- app/_containers/SkillsSection.jsx
+++ app/_containers/SkillsSection.jsx
@@ -1,7 +1,9 @@
 import skills from "@/app/_assets/pgarg.jpg";
 import { DiAptana } from "react-icons/di";
 import { BiFootball } from "react-icons/bi";
-import kanav from "@/app/_assets/kanav.jpg";
+//import kanav from "@/app/_assets/kanav.jpg";
+import i2 from '../../_assets/i2.jpg';
+import kanav from '../../_assets/kanav.jpg';
 import tushar from "@/app/_assets/tushar.png";
 import nic from "@/app/_assets/nic.png";
 import sahil from "@/app/_assets/sahil.jpg";
